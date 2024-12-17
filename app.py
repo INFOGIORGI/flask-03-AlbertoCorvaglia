7c228fcf78e644b817c38e4e2ec6d1808574261a
Alberto Corvaglia <99684434+AlbertoCorvaglia@users.noreply.github.com>
2024-12-17 11:03:04 +0000

    simone

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+prodotti = (("Tavolo","1","5"),("Sedia","1","15"),("PC","2","50"))
 
-app.run()
+@app.route("/")
+def home():
+    return render_template("index.html",titolo="HomePage")
+
+@app.route("/details")
+def details():
+    return render_template("details.html",titolo="Details",prodotti=prodotti)
+
+@app.route("/dettagliprodotto/<scaffale>")
+def dettagliprodotto(scaffale):
+    listaProdotti = []
+    print(scaffale)
+    for p in prodotti:
+        if(p[1]==scaffale):
+            listaProdotti.append(p)
+    return render_template("dettagliprodotto.html",prodotti=listaProdotti)
+
+app.run(debug=True)
